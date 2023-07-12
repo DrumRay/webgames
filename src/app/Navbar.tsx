c0eb4359fcb7493103c1e274b3f189d06ecd0423
DrumRay <drumray1408@gmail.com>
2023-07-12 14:07:30 +0300

    first commit laptop

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,11 @@
+import "./Navbar-styles.css"
+
+export default function Navbar() {
+    return (
+        <div className="Navbar-main">
+            <div>Logo</div>
+            <div>Navigation</div>
+            <div>social icons</div>
+        </div>
+    )
+}
