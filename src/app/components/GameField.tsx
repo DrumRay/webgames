4f15cedb4a84897211a3abbfc4693707f934492c
drumray <drumray1408@gmail.com>
2023-08-02 12:32:28 +0300

    немного доработал кнопки

--- src/app/components/GameField.tsx
+++ src/app/components/GameField.tsx
@@ -1,18 +1,28 @@
+"use client"
+
 import "../styles/GameField.css"
 import {FieldButton} from "../containers/Buttons"
+import {useState} from "react"
 
 export default function GameField() {
+    const [xTurn, setXTurn] = useState(true);
+    const [fieldData, setFieldData] = useState({  
+    0: "",
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+    5: "",
+    6: "",
+    7: "",
+    8: "",
+});
+
     return (
-    <div className="game_field">
-        <FieldButton/>
-        <FieldButton/>
-        <FieldButton/>
-        <FieldButton/>
-        <FieldButton/>
-        <FieldButton/>
-        <FieldButton/>
-        <FieldButton/>
-        <FieldButton/>
-    </div>
+        <div className="game_field">
+        {[...Array(9)].map((v, i) => (
+          <FieldButton key={i} /> 
+        ))}
+      </div>
     )
 }
--- src/app/containers/Buttons.tsx
+++ src/app/containers/Buttons.tsx
@@ -2,7 +2,7 @@ import "../styles/FieldButton.css";
 
 function FieldButton() {
     return (
-    <div className="w-[130px] h-[130px] relative bg-white bg-opacity-0">
+    <div className="w-[130px] h-[130px] relative bg-white bg-opacity-0 hover:cursor-pointer">
         <div className="w-[130px] h-[130px] left-0 top-0 absolute bg-zinc-700 rounded-md" />
         <div className="w-[120px] h-[120px] left-[5px] top-[5px] absolute bg-gradient-to-br from-fuchsia-400 to-zinc-600 rounded-md" />
     </div>
@@ -21,7 +21,7 @@ function NextPlayerButton() {
 
 function PlayerOrCompButton() {
     return (
-    <div className="px-2 py-1">
+    <div className="px-2 py-1 hover:cursor-pointer">
         <div className="w-[170px] h-[43px] p-[9px] bg-gradient-to-b from-purple-400 to-zinc-600 rounded-[7px] border-4 border-black justify-center items-center gap-2.5 inline-flex">
             <div className="w-[170px] h-[18px] text-center text-black text-[15px] font-semibold">Player Vs Player</div>
         </div>
@@ -31,7 +31,7 @@ function PlayerOrCompButton() {
 
 function RefreshButton() {
     return (
-    <div className="px-2 py-1">
+    <div className="px-2 py-1 hover:cursor-pointer">
         <div className="w-[42px] h-[42px] bg-gradient-to-b from-purple-400 to-zinc-600 rounded-[7px] border-4 border-black" />
     </div>
     )
